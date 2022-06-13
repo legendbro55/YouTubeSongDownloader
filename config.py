ede01a4a436fb7e540c7b4c23ed263b05dd520c7
legendbro55 <107189853+legendbro55@users.noreply.github.com>
2022-06-13 10:19:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 16267784
-    API_HASH = "" # 🤖 b29707434744713a3dad070bda43d856
-    BOT_TOKEN = "" # 🤖 5458034560:AAEWUHI6Swy6GGPJiPjcwwQgoYWRXcztRag
+    API_ID = "16267784"
+    API_HASH = "b29707434744713a3dad070bda43d856"
+    BOT_TOKEN = "5458034560:AAEWUHI6Swy6GGPJiPjcwwQgoYWRXcztRag"
